0c97773ecc724c6824f9f64f2c343e2bac9507ba
Kyle Ferriter <kylerferriter@gmail.com>
2018-09-06 11:34:56 -0400

    fix order of magnitude bug for disk requirement

--- src/toil/batchSystems/chronos.py
+++ src/toil/batchSystems/chronos.py
@@ -169,7 +169,7 @@ class ChronosBatchSystem(BatchSystemLocalSupport):
         disk = jobNode.disk / 2**20 # B -> MiB
         # set default here due to --default options not working when subset of reqs specified in workflow
         if not disk or disk < 20 * 2**30:
-            disk = 20 * 2**30
+            disk = 20 * 2**10 # MiB -> GiB
 
         logger.info("Requesting resources: mem={}, cpus={}, disk={}".format(mem, cpus, disk))
         # if a job with this name already exists, it will be overwritten in chronos.

89c1dccdc3ee96e80028580f2d89e582658d83bd
Kyle Ferriter <kylerferriter@gmail.com>
2018-09-12 02:04:39 -0400

    remove local mount

--- src/toil/batchSystems/chronos.py
+++ src/toil/batchSystems/chronos.py
@@ -208,9 +208,8 @@ class ChronosBatchSystem(BatchSystemLocalSupport):
             "arguments": [],
             "command": (
                 "sudo docker pull {};".format(self.toil_worker_image)
-                + "sudo docker run --net dcos --rm --privileged {} -v /local/{}:/tmp -v /toil-intermediate:/toil-intermediate {} _toil_worker '{}'".format(
+                + "sudo docker run --net dcos --rm --privileged {} -v /toil-intermediate:/toil-intermediate {} _toil_worker '{}'".format(
                         env_str, # aggregated environment vars
-                        str(uuid.uuid4()),
                         self.toil_worker_image,
                         " ".join(jobNode.command.split(" ")[1:])
                     ) # args after original _toil_worker
